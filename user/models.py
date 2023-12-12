10344b5b8c7e8f740beddc3b0699bb2057192153
Jodongjin <ehdwlsdlwkd@naver.com>
2023-12-12 13:51:22 +0900

    Feat: model def Users model 정의

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Users(models.Model):
+    uid = models.CharField(max_length=15, null=False)
+    name = models.CharField(max_length=15, null=False)
+    password = models.CharField(max_length=15, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.uid
