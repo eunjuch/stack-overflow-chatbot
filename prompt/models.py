81988932e7a860ad49836012374c7dc1240c94f0
gaheehee <jghgahee12@gmail.com>
2023-12-12 13:52:08 +0900

    BE-feature/history-models

--- prompt/models.py
+++ prompt/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class History(models.Model):
+    id = models.IntegerField(primary_key = True)
+    users_id = models.IntegerField()
+    title = models.CharField(max_length = 100)
+    file_name = models.CharField(max_length = 100)
+    is_file_exist = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+
+class Prompt(models.Model):
+    id = models.IntegerField(primary_key = True)
+    history_id = models.IntegerField()
+    user_message = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
