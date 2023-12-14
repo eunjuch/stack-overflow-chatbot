4d89363b15fd9e05d9a187d695668d5cbf99c210
gaheehee <jghgahee12@gmail.com>
2023-12-14 09:14:37 +0900

    chore

--- history/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'history'
-
-urlpatterns = [
-    path('/histories/prompts', views.PromptPostView.as_view(), name="prompt_create"),
-    path('histories/prompts/<int:prompt_id>', views.PromptDeleteView.as_view(), name="prompt_delete")
-]
