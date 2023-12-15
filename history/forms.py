ca4cb69212c9b2cea2c0105abe98c14096857b8f
Gahee <jghgahee12@gmail.com>
2023-12-15 17:06:00 +0900

    Revert "Backend"

--- history/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import History
-
-
-class HistoryForm(forms.ModelForm):
-    class Meta:
-        model = History
-        fields = ['user_id', 'title', 'file']
