e54e735a2e14cecb69df5e21131bdab189b5bfb3
jylee012 <jylee1@suresofttech.com>
2023-12-19 10:21:06 +0900

    Design: Add font

--- frontend/src/components/Sign/index.styles.js
+++ frontend/src/components/Sign/index.styles.js
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
 export const AuthInput = styled.input`
-  // width: 200px;
   width: 100%;
   height: 45px;
   background: #FFF;
   margin-top: 20px;
   padding: 0px 10px;
+  font-size: 14px;
 `
 
 export const AuthSubmit = styled.input`
-  // width: 200px;
   height: 45px;
   background: #041C24;
   color: #FFF;
@@ -25,9 +24,11 @@ export const CheckButton = styled.button`
   height: 45px;
   width: 100px;
   margin-top: 20px;
+  font-size: 14px;
 `
 
 export const ErrorMessage = styled.p`
   color: red;
   font-size: 13px;
+  margin: 0px;
 `
