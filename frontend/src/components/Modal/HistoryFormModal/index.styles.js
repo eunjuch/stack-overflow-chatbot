0fd9be87874d908e3d47fcc2cdeb925d2f4927e4
jylee012 <jylee1@suresofttech.com>
2023-12-19 10:21:32 +0900

    Design: change modal degisn

--- frontend/src/components/Modal/HistoryFormModal/index.styles.js
+++ frontend/src/components/Modal/HistoryFormModal/index.styles.js
@@ -7,6 +7,20 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 16px;
+
+  label {
+    font-size: 13px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    padding: 0px 5px;
+    margin-left: 5px;
+    background-color: #b8b8b8;
+    color: black;
+    cursor: pointer;
+    height: 45px;
+  }
 `;
 
 export const Button = styled.button`
@@ -14,4 +28,39 @@ export const Button = styled.button`
   height: 40px;
   background-color: black;
   color: white;
+  border-radius: 5px;
 `;
+
+export const HiddenInput = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip:rect(0,0,0,0);
+  border: 0;
+`
+
+export const FilenameInput = styled.input`
+  display: inline-block;
+  padding: 0px 10px;
+  font-family: inherit;
+  line-height: normal;
+  vertical-align: middle;
+  background-color: #FFF;
+  text-overflow: ellipsis;
+  width: 100%;
+  border-radius: 5px;
+  height: 45px;
+  color: black;
+
+  &::placeholder { 
+    color: lightgray;
+  }
+`
+
+export const FileInputWrapper = styled.div`
+  display: flex;
+  width: 80%;
+`
